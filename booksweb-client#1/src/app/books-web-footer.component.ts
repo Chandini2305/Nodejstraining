6ea6af78638f41a9efc3e5e1c33c1f719136f8f3
Chandini2305 <69888517+Chandini2305@users.noreply.github.com>
2020-09-01 17:13:00 +0530

    assignment-6

---
+++ booksweb-client#1/src/app/books-web-footer.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core'
+ 
+@Component({
+    selector: 'app-booksweb-footer',  //custom html element or component identifier
+    template:
+    `
+    <h1>Footer</h1>
+   
+    `
+})
+export class BooksWebFooterComponent{
+    
+}
